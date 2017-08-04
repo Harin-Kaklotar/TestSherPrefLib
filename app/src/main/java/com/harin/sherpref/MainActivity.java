7b1a6f33fee98d43c943d920c16b45825eb3af6d
Harin Kaklotar <harinlive@gmail.com>
2017-08-04 19:52:27 +0530

    change textview message

--- app/src/main/java/com/harin/sherpref/MainActivity.java
+++ app/src/main/java/com/harin/sherpref/MainActivity.java
@@ -2,12 +2,18 @@ package com.harin.sherpref;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView helloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloWorld = (TextView) findViewById(R.id.lbl_hello_world);
+        helloWorld.setText("Hello Harin, welcome to version control !!!");
     }
 }
